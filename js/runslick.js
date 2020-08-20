be963c027de7b4dc02a0d684ae316a594cd2e26e
nealpatel <nealpatel2021@gmail.com>
2020-08-20 19:49:16 -0400

    fixed oopsie, refactored my js

---
+++ js/runslick.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('#slides').slick({
+    infinite: true,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  });
+});
