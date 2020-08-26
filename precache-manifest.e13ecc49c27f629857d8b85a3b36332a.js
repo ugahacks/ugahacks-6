2e046a1c339b5afc6dc5c636e52fd36c425d2bf4
nealpatel <nealpatel2021@gmail.com>
2020-08-26 19:31:27 -0400

    Updates

---
+++ precache-manifest.e13ecc49c27f629857d8b85a3b36332a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f42a612390c9d8ea6b2e21de4b905bce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9a70e286cc05f8794c5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6e074b59059a2ce3ce1b",
+    "url": "/static/js/2.286c546a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.286c546a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9a70e286cc05f8794c5",
+    "url": "/static/js/main.f3cc58dc.chunk.js"
+  },
+  {
+    "revision": "1732638071bab48d3055",
+    "url": "/static/js/runtime-main.dc2f0862.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
