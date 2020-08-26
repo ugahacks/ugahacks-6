78da96f7673368baa2dedb76cceab289f3096165
nealpatel <nealpatel2021@gmail.com>
2020-08-26 19:22:03 -0400

    added pages.js for easy of publishing builds to ugahacks-6

---
+++ pages.js
@@ -0,0 +1,15 @@
+const ghpages = require("gh-pages");
+const pathname = `${__dirname}/build`;
+const repoURL = "https://github.com/ugahacks/ugahacks-6.git";
+
+ghpages.publish(
+  pathname,
+  {
+    branch: "react-hacks-build",
+    repo: repoURL,
+  },
+  (err) => {
+    if (err) console.log("ERROR: ", err);
+    else console.log("PUBLISHED");
+  }
+);
