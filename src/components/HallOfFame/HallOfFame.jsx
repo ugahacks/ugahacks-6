61bbeb2af8b37ab8065f716bf47179a2c33477cc
nealpatel <nealpatel2021@gmail.com>
2020-08-27 19:51:48 -0400

    sexy ass but broken HOF carousel

--- src/components/HallOfFame/HallOfFame.jsx
+++ src/components/HallOfFame/HallOfFame.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
+
+import AwesomeSlider from 'react-awesome-slider';
+// import AwsSliderStyles from 'react-awesome-slider/src/styles.scss';
 import './HallOfFame.css';
+import 'react-awesome-slider/dist/styles.css';
+import 'react-awesome-slider/dist/custom-animations/cube-animation.css';
 
-function HallOfFame(props) {
-  return (
-    <>
-      <section className=''>
+import team_wand from "./media/wand.png"
 
-      </section>
-    </>
 
-  );
+var path = "./media/wand.png"
+
+
+class HallOfFame extends React.Component { //function HallOfFame(props) {
+  render() {
+    return (
+      <div id="HallOfFame" className="">
+        <AwesomeSlider
+          animation="cubeAnimation"
+        >
+          <div data-src={team_wand} />
+          <div data-src={team_wand} />
+          <div data-src={team_wand} />
+        </AwesomeSlider>
+      </div>
+    );
+  }
 }
 
+// ReactDOM.render(<Carousel />, document.querySelector('.demo-carousel'));
 export default HallOfFame;
