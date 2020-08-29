e8d6e194fcf91772b4e747ab27d0e9f50012b0be
Kane Du <kanedu2000@gmail.com>
2020-08-29 16:57:16 -0400

    more changes

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -1,101 +1,171 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Schedule.css';
 import '../../bootstrap.css'
+import { motion } from "framer-motion"
+
+
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+  }
+}
 
 
 function Schedule(props) {
+
+  const dayMapping = {
+    0: "friday",
+    1: "saturday",
+    2: "sunday",
+  }
+
+  const [day, setDay] = useState(getDay());
+
+  function getDay() {
+    var date = new Date();
+    var dateDay = date.getDay();
+
+    if (dateDay === 6) {
+      return 1;
+    } else if( dateDay === 0) {
+      return 2;
+    } else {
+      return 0;
+    }
+  }
+
+  function renderDay() {
+    if (day === 0) {
+      return (
+
+        <div >
+          <table>
+            <tbody>
+              <tr>
+                <td>What's big data? - Example Sponsor (Room 101)</td>
+                <td>9:00 am</td>
+              </tr>
+              <tr>
+                <td>Workshops</td>
+                <td>10:00 am</td>
+              </tr>
+              <tr>
+                <td>More Workshops</td>
+                <td>11:00 am</td>
+              </tr>
+              <tr>
+                <td>Even More Workshops</td>
+                <td>11:30 am</td>
+              </tr>
+              <tr>
+                <td>Lunch</td>
+                <td>12:00 pm</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+      );
+    } else if (day === 1) {
+      return (
+
+        <div >
+          <table>
+            <tbody>
+              <tr>
+                <td>Breakfast</td>
+                <td>8:00 am</td>
+              </tr>
+              <tr>
+                <td>Workshops</td>
+                <td>10:00 am</td>
+              </tr>
+              <tr>
+                <td>The latest Byte - Example Sponsor (Room 101)</td>
+                <td>11:00 am</td>
+              </tr>
+              <tr>
+                <td>Even More Workshops</td>
+                <td>11:30 am</td>
+              </tr>
+              <tr>
+                <td>Lunch</td>
+                <td>12:00 pm</td>
+              </tr>
+              <tr>
+                <td>Cup Stacking - Example Sponsor (Room 101)</td>
+                <td>1:00 pm</td>
+              </tr>
+              <tr>
+                <td>Dinner</td>
+                <td>6:00 pm</td>
+              </tr>
+              <tr>
+                <td>Join the UGAHacks team!</td>
+                <td>7:00 pm</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+      );
+    } else {
+      return (
+
+        <div >
+          <table>
+            <tbody>
+              <tr>
+                <td>Brekafast</td>
+                <td>9:00 am</td>
+              </tr>
+              <tr>
+                <td>Judging</td>
+                <td>10:00 am</td>
+              </tr>
+              <tr>
+                <td>Closing Ceremony</td>
+                <td>1:00 pm</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+      );
+    }
+  }
+
+  function changeDay(increment) {
+    if (day + increment >= 0 && day + increment <= 2) {
+      setDay(day+increment);
+    }
+  }
+
   return (
     <>
       <section id='schedule' className=''>
         <div className="section-text schedule-text">
           <h1>Schedule</h1>
-          <div className="day-schedule collapse" id="Frisched">
-            <h1 className="day-header"><span className="disabled"><i className="material-icons">arrow_back_ios</i></span> Friday <span className="active" onClick="changeSchedule('#Frisched','#Satsched')"><i className="material-icons">arrow_forward_ios</i></span></h1>
-            <table>
-              <tbody>
-                <tr>
-                  <td>What's big data? - Example Sponsor (Room 101)</td>
-                  <td>9:00 am</td>
-                </tr>
-                <tr>
-                  <td>Workshops</td>
-                  <td>10:00 am</td>
-                </tr>
-                <tr>
-                  <td>More Workshops</td>
-                  <td>11:00 am</td>
-                </tr>
-                <tr>
-                  <td>Even More Workshops</td>
-                  <td>11:30 am</td>
-                </tr>
-                <tr>
-                  <td>Lunch</td>
-                  <td>12:00 pm</td>
-                </tr>
-              </tbody>
-            </table>
+          <div className="day-header">
+            <motion.button
+              onClick={() => changeDay(-1)}
+              className="back-button"
+              variants={buttonVariants}
+              whileHover="hover"
+              > </motion.button>
+            <h1>{dayMapping[day]} </h1>
+            <motion.button
+              onClick={() => changeDay(1)}
+              className="forward-button"
+              variants={buttonVariants}
+              whileHover="hover"
+              > </motion.button>
           </div>
-          <div className="day-schedule collapse" id="Satsched">
-            <h1 className="day-header"><span className="active" onClick="changeSchedule('#Satsched','#Frisched')"><i className="material-icons">arrow_back_ios</i></span> Saturday <span className="active" onClick="changeSchedule('#Satsched','#Sunsched')"><i className="material-icons">arrow_forward_ios</i></span></h1>
-            <table>
-              <tbody>
-                <tr>
-                  <td>Breakfast</td>
-                  <td>8:00 am</td>
-                </tr>
-                <tr>
-                  <td>Workshops</td>
-                  <td>10:00 am</td>
-                </tr>
-                <tr>
-                  <td>The latest Byte - Example Sponsor (Room 101)</td>
-                  <td>11:00 am</td>
-                </tr>
-                <tr>
-                  <td>Even More Workshops</td>
-                  <td>11:30 am</td>
-                </tr>
-                <tr>
-                  <td>Lunch</td>
-                  <td>12:00 pm</td>
-                </tr>
-                <tr>
-                  <td>Cup Stacking - Example Sponsor (Room 101)</td>
-                  <td>1:00 pm</td>
-                </tr>
-                <tr>
-                  <td>Dinner</td>
-                  <td>6:00 pm</td>
-                </tr>
-                <tr>
-                  <td>Join the UGAHacks team!</td>
-                  <td>7:00 pm</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <div className="day-schedule collapse" id="Sunsched">
-            <h1 className="day-header"><span className="active" onClick="changeSchedule('#Sunsched','#Satsched')"><i className="material-icons">arrow_back_ios</i></span> Sunday <span className="disabled"><i className="material-icons">arrow_forward_ios</i></span></h1>
-            <table>
-              <tbody>
-                <tr>
-                  <td>Brekafast</td>
-                  <td>9:00 am</td>
-                </tr>
-                <tr>
-                  <td>Judging</td>
-                  <td>10:00 am</td>
-                </tr>
-                <tr>
-                  <td>Closing Ceremony</td>
-                  <td>1:00 pm</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+
+          {renderDay()}
         </div>
       </section>
+
     </>
 
   );
