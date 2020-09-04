8dbd3cb55681fd5449d9c996a08fa74f12077782
Andrei Spatariu <andros415@gmail.com>
2020-09-04 14:47:57 -0400

    Added mobile splash background, header and register button

--- src/components/Splash/Splash.jsx
+++ src/components/Splash/Splash.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './Splash.css';
 import sixLogo from './images/sixlogo-01.svg'
+import Button from '@material-ui/core/Button';
 
 function Splash(props) {
   return (
     <>
       <section className='splash'>
         <img className='logo' src={sixLogo}/>
+        <div className='headercontainer'>
+        <h2>February 5-7, 2021 @ MLC</h2>
+        <Button variant="contained" size="large">Register Now</Button>
+        </div>
       </section>
     </>
 
