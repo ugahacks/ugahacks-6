3ffa17a0d7f380d1a699ef3a3e1f08793a48bc26
nealpatel <nealpatel2021@gmail.com>
2020-09-07 18:19:16 -0400

    added foundational hooks and states.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { withRouter } from "react-router";
+import React, { useState } from 'react';
 import './Nav.css';
 import { motion } from "framer-motion";
 import { withStyles } from '@material-ui/core/styles';
 import Tooltip from '@material-ui/core/Tooltip';
+
+import useDocumentScrollThrottled from './useDocumentScrollThrottled';
+
 
 const CustomizedTooltip = withStyles({
   tooltip: {
@@ -30,101 +32,32 @@ const InvertedTooltip = withStyles({
   }
 })(Tooltip);
 
-export default class DynamicNav extends React.Component {
-  constructor(props) {
-    super(props);
-    this.listener = null;
-    this.state = {
-      status: "+",
-      width: 0,
-      height: 0,
-    };
-    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-  }
-
-  componentDidMount() {
-    this.updateWindowDimensions();
-    window.addEventListener('resize', this.updateWindowDimensions);
-
-    this.listener = document.addEventListener("scroll", e => {
-      var scrolled = document.scrollingElement.scrollTop;
-      // if (scrolled >= 120) {
-      //   if (this.state.status !== "amir") {
-      //     this.setState({ status: "amir" });
-      //   }
-      // } else {
-      //   if (this.state.status !== "top") {
-      //     this.setState({ status: "top" });
-      //   }
-      // }
-      console.log(scrolled, this.state.width, this.state.height);
-    });
-  }
-
-  updateWindowDimensions() {
-    this.setState({ width: window.innerWidth, height: window.innerHeight });
-  }
-
-  componentDidUpdate() {
-    document.removeEventListener("scroll", this.listener);
-  }
-
-  render() {
-    return (
-      <nav>
-        <div className="dotstyle dotstyle-fillin">
-          <div>
-            
-            <div>
-              <CustomizedTooltip title="Home" placement="right" arrow>
-                <a href="#splash" className="current">Home</a>
-              </CustomizedTooltip>
-            </div>
-
-            <div>
-              <CustomizedTooltip title="About" placement="right" arrow>
-                <a href="#about">About</a>
-              </CustomizedTooltip>
-            </div>
-
-            <div>
-              <CustomizedTooltip title="Schedule" placement="right" arrow>
-                <a href="#schedule">Schedule</a>
-              </CustomizedTooltip>
-            </div>
-
-            <div>
-              <CustomizedTooltip title="FAQ" placement="right" arrow>
-                <a href="#faq">FAQ</a>
-              </CustomizedTooltip>
-            </div>
-
-            <div>
-              <CustomizedTooltip title="Covid FAQ" placement="right" arrow>
-                <a href="#covidfaq">Covid FAQ</a>
-              </CustomizedTooltip>
-            </div>
-
-            <div>
-              <CustomizedTooltip title="Sponsors" placement="right" arrow>
-                <a href="#sponsors">Sponsors</a>
-              </CustomizedTooltip>
-            </div>
-
-          </div>
-        </div>
-      </nav>
-    );
-  }
-
-}
-
-/*
 function Nav(props) {
+  const [goLight, setGoLight] = useState(false);
+  const [goDark, setGoDark] = useState(false);
+
+  const MINIMUM_SCROLL = 0;
+  const TIMEOUT_DELAY = 400;
+
+  useDocumentScrollThrottled(callbackData => {
+    const { previousScrollTop, currentScrollTop } = callbackData;
+    const isScrolledDown = previousScrollTop < currentScrollTop;
+    const isMinimumScrolled = currentScrollTop > MINIMUM_SCROLL;
+
+    setGoLight(currentScrollTop > 2);
+
+    setTimeout(() => {
+      setGoDark(isScrolledDown && isMinimumScrolled);
+    }, TIMEOUT_DELAY);
+  });
+  
+  /* These will be used to set dynamic classNames */
+  const lightStyle = goLight ? 'light' : ''; 
+  const darkStyle = goDark ? 'dark' : '';
+
   return (
-    <>
     <nav>
-      <div className="dotstyle dotstyle-fillin">
+      <div className={`dotstyle dotstyle-fillin ${lightStyle} ${darkStyle}`}>
         <div>
           
           <div>
@@ -166,9 +99,7 @@ function Nav(props) {
         </div>
       </div>
     </nav>
-    </>
   );
 }
 
 export default Nav;
-*/
