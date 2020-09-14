f420fb84c9fa85494971b99e4d8169a9c777366a
nealpatel <nealpatel2021@gmail.com>
2020-09-14 19:51:42 -0400

    sponsor section look good now.

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -7,12 +7,11 @@ function Sponsors(props) {
     <>
       <section className='section sponsors-section' id="sponsors">
         <div className="sponsors-container">
-          <h1>Sponsors</h1>
           <div className="packet-text">
             <h3>Interested in sponsoring us?</h3>
             <p>By sponsoring UGAHacks, your company will have the opportunity to inspire new programmers while also reaching
             out to and recruiting the rising stars of the tech industry.</p>
-            <Button variant="contained" size="large"><a href="https://www.ugahacks.com/sponsorship-packet"> Sponsorship Packet</a></Button>
+            <Button variant="contained" size="large"><a href="https://www.ugahacks.com/sponsorship-packet">See Our Sponsorship Packet</a></Button>
             <br></br>
             <Button variant="contained" size="large"><a href="mailto:sponsor@ugahacks.com">Contact our sponsorship team</a></Button>
           </div>
