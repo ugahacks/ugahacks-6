6a2624f1368805316a2d3cabb4e572bfcfc7d187
nealpatel <nealpatel2021@gmail.com>
2020-09-14 20:56:14 -0400

    color layout and content/

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -4,21 +4,18 @@ import Button from '@material-ui/core/Button';
 
 function Sponsors(props) {
   return (
-    <>
-      <section className='section sponsors-section' id="sponsors">
-        <div className="sponsors-container">
-          <div className="packet-text">
-            <h3>Interested in sponsoring us?</h3>
-            <p>By sponsoring UGAHacks, your company will have the opportunity to inspire new programmers while also reaching
-            out to and recruiting the rising stars of the tech industry.</p>
-            <Button variant="contained" size="large"><a href="https://www.ugahacks.com/sponsorship-packet">See Our Sponsorship Packet</a></Button>
-            <br></br>
-            <Button variant="contained" size="large"><a href="mailto:sponsor@ugahacks.com">Contact our sponsorship team</a></Button>
-          </div>
+    <section className='section sponsors-section' id="sponsors">
+      <div className="sponsors-container">
+        <div className="packet-text">
+          <h3>Interested in sponsoring us?</h3>
+          <p>By sponsoring UGAHacks, your company will have the opportunity to inspire new programmers while also reaching
+          out to and recruiting the rising stars of the tech industry.</p>
+          <Button variant="contained" size="large"><a href="https://www.ugahacks.com/sponsorship-packet">See Our Sponsorship Packet</a></Button>
+          <br></br>
+          <Button variant="contained" size="large"><a href="mailto:sponsor@ugahacks.com">Contact our sponsorship team</a></Button>
         </div>
-      </section>
-    </>
-
+      </div>
+    </section>
   );
 }
 
