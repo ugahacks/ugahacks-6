b124853ce1ac0718939460d6632a17b42a799dad
Neal Patel (nealdotpy) <nealpatel2021@gmail.com>
2020-10-05 12:22:19 -0400

    refactored and cleaned.

--- src/App.js
+++ src/App.js
@@ -15,59 +15,9 @@ import Footer from './components/Footer/Footer.jsx';
 import ReactFullPage from '@fullpage/react-fullpage';
 import { useMediaQuery } from 'react-responsive'
 
-/*
-function App() {
-  hotjar.initialize(1566634,6);
-  if (!isMobile) {
-    return(
-      <div className="App">
-        <ReactFullPage
-          debug
-          navigation = {true}
-          navigationPosition = {'left'}
-          navigationTooltips = {['Splash', 'About', 'Schedule', 'FAQ', 'COVID FAQ', 'Sponsors']}
-          licenseKey = {'OPEN-SOURCE-GPLV3-LICENSE'}
-          // menu = {'#navbar'}
-          scrollingSpeed = {1000}
-          normalScrollElements = {'.faq-question-wrapper'} // :)
-          // anchors={['splash', 'about', 'schedule', 'faq', 'covidfaq', 'sponsors']}
-
-          render={comp => (
-              <ReactFullPage.Wrapper>
-                <Splash/>
-                <About/>
-                <Schedule/>
-                <FAQ/>
-                <CovidFAQ/>
-                <Sponsors/>
-                <Footer/>
-              </ReactFullPage.Wrapper>
-          )}
-        />
-      </div>
-    );
-  } else {
-    return(
-      <div className="App">
-        <Nav/>
-        <Splash/>
-        <About/>
-        <Schedule/>
-        <FAQ/>
-        <CovidFAQ/>
-        <Sponsors/>
-        <Footer/>
-      </div>
-    );
-  }
-}
-*/
-
 
 function App() {
   hotjar.initialize(1566634,6);
-
-  const isTabletOrMobileDevice = useMediaQuery({ maxDeviceWidth: 1224 })
 
   const Desktop = ({ children }) => {
     const isDesktop = useMediaQuery({ minWidth: 1439 });
@@ -77,14 +27,6 @@ function App() {
   const Tablet = ({ children }) => {
     const isTablet = useMediaQuery({ minWidth: 50, maxWidth: 1438 })
     return isTablet ? children : null
-  }
-  const Mobile = ({ children }) => {
-    const isMobile = useMediaQuery({ maxWidth: 767 })
-    return isMobile ? children : null
-  }
-  const Default = ({ children }) => {
-    const isNotMobile = useMediaQuery({ minWidth: 768 })
-    return isNotMobile ? children : null
   }
 
   return(
