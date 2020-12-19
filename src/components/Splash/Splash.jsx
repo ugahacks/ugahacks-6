40e28eb8ed252747c6d216a875c47d960ab33f26
Andrei Spatariu <andros415@gmail.com>
2020-12-19 13:16:22 -0500

    Fix warning on _blank link

--- src/components/Splash/Splash.jsx
+++ src/components/Splash/Splash.jsx
@@ -92,7 +92,7 @@ function Splash(props) {
   return (
     <>
       <section className="section splash" id="splash">
-      <a id="mlh-trust-badge" href="https://mlh.io/seasons/2021/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2021-season&utm_content=white" target="_blank"><img src="https://s3.amazonaws.com/logged-assets/trust-badge/2021/mlh-trust-badge-2021-white.svg" alt="Major League Hacking 2021 Hackathon Season"></img></a>
+      <a id="mlh-trust-badge" href="https://mlh.io/seasons/2021/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2021-season&utm_content=white" target="_blank" rel="noopener noreferrer"><img src="https://s3.amazonaws.com/logged-assets/trust-badge/2021/mlh-trust-badge-2021-white.svg" alt="Major League Hacking 2021 Hackathon Season"></img></a>
         <div className="headercontainer">
           <img className="logo" src={sixLogo} alt="UGAHacks 6 Logo"/>
           <Countdown/>
