c22d8b3faa576833e4d119d50a9a9bae65a4b3d7
Andrei Spatariu <andros415@gmail.com>
2021-02-03 20:45:48 -0500

    Fixing some overlap issues with Tracks

--- src/components/Tracks/Tracks.jsx
+++ src/components/Tracks/Tracks.jsx
@@ -11,13 +11,12 @@ function Tracks(props) {
       <section className="section tracks-section" id="tracks">
         <div className="tracks-container">
           <div className="tracks-text-container">
-            <h1>Tracks and Challenges</h1>
+            <h1>Tracks & Challenges</h1>
             <p>Tracks are a certain focus for our hackathon this year that will have specific mentor help, workshops, times, and special prizes if a project does align with that focus.
             Our Track this year is <b>Hardware!</b> Builder Byte is excited to see what you will build using different forms of hardware technology such as arduino kits, raspberry pis, etc!
             UGAHacks will not be providing any hardware materials this year, but you can purchase your own materials and your group may be eligible for a reimbursement from UGAHacks. More details can be found on page 14 of the pre-event guide!</p>
             <p>In addition to tracks, some sponsors will even have their own challenges geared around a specific theme/tool for hackers to participate in.
             <b> There may even be a chance to score some sweet prizes!</b></p>
-            <p>Stay tuned to the opening cermony where more details will be revealed!</p>
             <Button className="show-more-black" variant="contained" size="large" id="show-more" href="https://6.ugahacks.com/static/media/pre_event_packet.091f31bd.pdf" target="_blank">Check Out Our Pre-event Guide</Button>
           </div>
           <img className="winner-byte" alt="Winner Byte" src={winnerByte}/>
